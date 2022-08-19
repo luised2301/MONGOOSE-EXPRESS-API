15933696585c2872f3d07c7a78a54f0a8210077e
luised2301 <luised2301@gmail.com>
2022-08-18 22:01:15 -0500

    post and get added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const mongoose = require("mongoose")
 const express = require("express")
-
+//App
 const app = express()
 
+
+//Middlewares (lo estamos declarando de manera global por el app.use)
+app.use(express.json())
+//Desestructurando la informacion de .env
 const {
     DB_USERNAME,
     DB_PASSWORD,
@@ -11,11 +15,40 @@ const {
     DB_NAME,    
 }= process.env
 
+
+//Schema -> Document (cada entrada es un documento)
+const koderSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength:20,
+    },
+    modulo:{
+        type:String
+    },
+    edad:{
+        type: Number,
+        min: 18,
+        max:150
+    },
+    generacion:{
+        type:String,
+        required:true
+    },
+    sexo:{
+        type: String,
+        enum: ["f", "m", "o"]
+    }
+})
+
+// Modelos -> Collection
+const Koder = mongoose.model("koders", koderSchema)
+
+//Creando mi url de mongo
 const URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DBHOST}${DB_NAME}`
 
-
-console.log("process env", process.env)
-
+//Conectamos a la url (esto es una promesa)
 mongoose.connect(URL)  
 .then(()=>{
     console.log("conectado a la base de datos de mongo")
@@ -26,4 +59,69 @@ mongoose.connect(URL)
 })
 .catch((error)=>{
     console.log("error",error)
+})
+
+
+//Creamos un endpoint
+app.post ("/koders", async (request, response)=>{
+    //Desestructurando la request
+    const {body}= request
+    try{
+        //Accedemos a la base de datos ->promesas
+        const koder = await Koder.create(body)
+        console.log("koder",koder)
+        response.status(201)
+        response.json({
+            success:true,
+            data:{koder}
+        })
+    }
+    catch(error){
+        console.log("error:",error)
+        response.status(400)
+        response.json({
+            success:false,
+            message: error.message
+        })
+    }
+
+})
+
+//Encontrar un koder por su id
+//Usando Model.findByID
+
+
+app.get ("/koders", async (request, response)=>{
+    //Desestructurando la request
+    const {query}=request
+    console.log("user to find:",query.id)
+    try{
+        //Accedemos a la base de datos ->promesas
+         const koder = await Koder.findById(query.id)
+         if(!koder){
+            console.log("koder not found")
+            response.status(400)
+            response.json({
+             success:false,
+             message: "no match found"
+        })
+        return
+         }
+         else
+         console.log("koder",koder)
+         response.status(201)
+         response.json({
+             success:true,
+             data:{koder}
+        })
+    }
+    catch(error){
+        console.log("error:",error)
+        response.status(400)
+         response.json({
+             success:false,
+             message: error.message
+        })
+    }
+
 })
